8106de9b9ab528e93eba12d316af62a5826ed017
Matthias Sieber <matthiasksieber@gmail.com>
2021-09-07 20:46:57 -0700

    initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# Blace.io - Cardano Marketplace Creator
+
+This is the public facing website for blace.io.
+Support our [proposal](https://cardano.ideascale.com/a/dtd/368797-48088)!
---
+++ src/App.vue
@@ -0,0 +1,38 @@
+<template>
+  <header class="relative flex items-center justify-center h-screen mb-12 overflow-hidden">
+    <video autoplay loop muted class="absolute z-10 w-auto min-w-full min-h-full max-w-none">
+      <source src="blaceio.mp4" type="video/mp4" />Your browser does not support the video tag.
+    </video>
+  </header>
+  <div class="max-w-lg m-auto">
+    <h1 class="text-2xl font-mono">Blace.io</h1>
+    <h2 class="text-xl mb-4">The Cardano Marketplace Creator</h2>
+    <p class="mb-4">
+      How can we create a positive developer experience that helps the developer focus on building successful decentralized applications?
+    </p>
+    <p class="mb-4">
+      This is the leading question that led us to the development of <strong>blace</strong>.
+    </p>
+    <p class="mb-4">
+      Numerous projects are being built or proposed in <a href="https://cardano.ideascale.com/" target="_blank">Project Catalyst</a> that aim to create decentralized marketplaces to serve as intermediaries between supply and demand, sellers and buyers, server and clients.
+We have identified the need to build a framework in order for people to spin-up their marketplaces.
+Just like their simpler Web 2.0 equivalents, blace enables scaffolding marketplaces with a simple <span class="font-mono bg-gray-500 text-white px-1">blace new</span>, thus cutting down development time and cost.
+    </p>
+    <p class="mb-12">
+      Please consider <a href="https://cardano.ideascale.com/a/dtd/368797-48088">supporting our proposal</a> as we are heading into a new development phase. There you can also read more about our <a href="https://winwin-team.web.app/" target="_blank">team</a> and specific use-cases.
+    </p>
+  </div>
+</template>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
+}
+
+a {
+  color: blue !important;
+}
+</style>
